8afd78e2aa7696a3b08237f0383631b8fd1ceee3
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-11-11 23:51:48 +0900

    add ipynb

---
+++ Natural_language_processing/practice/Mecab.py
@@ -0,0 +1,18 @@
+import os
+import Mecab
+
+
+
+class MeCab():
+    def __init__(self):
+        self.TrainDataUrl = "../aozora/train_data"
+        self.TestDataUrl = "../aozora/test_data"
+
+    def FileRead(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    main()
+
